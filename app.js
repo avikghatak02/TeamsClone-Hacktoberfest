e3862d77373a4853ebaac8f377621c96e253adaa
Khushi Singh <skhushii001@gmail.com>
2021-06-23 02:41:39 +0530

    added socket.io

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
+const { Socket } = require('dgram');
 const express = require('express');
-const app = express();                              //Initialize the express application
+const app = express();                                            //Initialize the express application
 const server = require('http').Server(app);
-const { v4: uuidv4 } = require('uuid');             //Require UUID to generate unique room ids
-app.set('view engine', 'ejs');                      //Setting the view engine as ejs
+const { v4: uuidv4 } = require('uuid');                           //Require UUID to generate unique room ids
+const io = require('socket.io')(server)                           //Require Socket.io 
+app.set('view engine', 'ejs');                                    //Setting the view engine as ejs
 app.use(express.static('public'));
 //ROUTES===============================================================================================================================
-app.get('/', (req, res) => {                        //Landing Route : Redirects to a unique room
+app.get('/', (req, res) => {                                      //Landing Route : Redirects to a unique room
     res.redirect(`/${uuidv4()}`);
 })
 
-app.get('/:room', (req, res) => {                   //Unique Room Route : Passes the unique room id to ejs file
+app.get('/:room', (req, res) => {                                 //Unique Room Route : Passes the unique room id to ejs file
     res.render('index', { roomId: req.params.room });
 });
 
-
+io.on('connection', Socket => {                                   //Join Room with Room ID
+    Socket.on('join-room', (roomId) => {
+        Socket.join(roomId);
+        Socket.broadcast.to(roomId).emit('user-connected');       //Broadcast User is Connected to all clients except sender
+    })
+})
 //SERVER ==============================================================================================================================
 server.listen(3000, () => {
     console.log('Serving on Port 3000')
--- public/script.js
+++ public/script.js
@@ -1,5 +1,9 @@
+//import { Socket } from 'dgram';
+const socket = io('/')
+
 const videoGrid = document.getElementById('video-grid');   //Get the div where video will be displayed 
 const ownVideo = document.createElement('video');          //Creates a video element for the video of caller 
+
 ownVideo.muted = true;
 
 let ownVideoStr                                            //Video Stream of the Caller
@@ -11,10 +15,21 @@ navigator.mediaDevices.getUserMedia({                      //Get access to audio
     addVideoStr(ownVideo, stream);
 })
 
+socket.emit('join-room', RoomId);
+
+socket.on('user-connected', () => {                        //Connect new user when user-connected is broadcasted by sender
+    connectUser();
+})
+
+//VIDEO FUNCTIONS =================================================================================================================
 const addVideoStr = (video, stream) => {                   //Function to add video stream after loading all the media
     video.srcObject = stream;
     video.addEventListener('loadedmetadata', () => {
         video.play();
     })
     videoGrid.append(video);                               //Append the video to be displayed
+}
+
+const connectUser = () => {                                //Function to connect new user 
+    console.log('new user');
 }
