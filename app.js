6bb530b606494bbde5da88eddf977b376f9366c9
Khushi Singh <skhushii001@gmail.com>
2021-06-24 01:47:05 +0530

    added second user

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const { Socket } = require('dgram');
+// const { Socket } = require('dgram');
 const express = require('express');
 const app = express();                                            //Initialize the express application
 const server = require('http').Server(app);
 const { v4: uuidv4 } = require('uuid');                           //Require UUID to generate unique room ids
 const io = require('socket.io')(server)                           //Require Socket.io 
+const { ExpressPeerServer } = require('peer');                    //Peer Server
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
 app.set('view engine', 'ejs');                                    //Setting the view engine as ejs
 app.use(express.static('public'));
+app.use('/peerjs', peerServer);                                    //Specify url for peer js
+
 //ROUTES===============================================================================================================================
 app.get('/', (req, res) => {                                      //Landing Route : Redirects to a unique room
     res.redirect(`/${uuidv4()}`);
@@ -16,9 +22,9 @@ app.get('/:room', (req, res) => {                                 //Unique Room 
 });
 
 io.on('connection', Socket => {                                   //Join Room with Room ID
-    Socket.on('join-room', (roomId) => {
+    Socket.on('join-room', (roomId, userId) => {
         Socket.join(roomId);
-        Socket.broadcast.to(roomId).emit('user-connected');       //Broadcast User is Connected to all clients except sender
+        Socket.broadcast.to(roomId).emit('user-connected', userId);  //Broadcast User is Connected to all clients except sender
     })
 })
 //SERVER ==============================================================================================================================
--- public/script.js
+++ public/script.js
@@ -1,10 +1,15 @@
-//import { Socket } from 'dgram';
 const socket = io('/')
 
 const videoGrid = document.getElementById('video-grid');   //Get the div where video will be displayed 
 const ownVideo = document.createElement('video');          //Creates a video element for the video of caller 
 
 ownVideo.muted = true;
+
+const peer = new Peer(undefined, {                         //Create a new peer connection
+    path: '/peerjs',
+    host: '/',
+    port: '3000'
+});
 
 let ownVideoStr                                            //Video Stream of the Caller
 navigator.mediaDevices.getUserMedia({                      //Get access to audio & video 
@@ -13,23 +18,45 @@ navigator.mediaDevices.getUserMedia({                      //Get access to audio
 }).then(stream => {
     ownVideoStr = stream;
     addVideoStr(ownVideo, stream);
-})
 
-socket.emit('join-room', RoomId);
+    peer.on('call', call => {                              //Answering the call from the sender
+        console.log('hi');
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideo => {
+            addVideoStr(video, userVideo);
+        });
+    }, err => { console.log(err); })
 
-socket.on('user-connected', () => {                        //Connect new user when user-connected is broadcasted by sender
-    connectUser();
+    socket.on('user-connected', (userId) => {              //Connect new user when user-connected is broadcasted by sender
+        setTimeout(() => {
+            //user joined
+            connectUser(userId, stream);
+        }, 1000)
+    })
+});
+
+peer.on('open', id => {                                //Listen on peer connection (id: id os new user)
+    socket.emit('join-room', RoomId, id);
 })
 
 //VIDEO FUNCTIONS =================================================================================================================
+
+const connectUser = (userId, stream) => {                  //Function to connect new user 
+    const call = peer.call(userId, stream)                 //Calling the other user(client)
+    const video = document.createElement('video')          //Create a new video element to be shown for another user
+    call.on('stream', userVideo => {
+        addVideoStr(video, userVideo)
+    })
+    call.on('close', () => {
+        video.remove();
+    })
+}
+
 const addVideoStr = (video, stream) => {                   //Function to add video stream after loading all the media
     video.srcObject = stream;
     video.addEventListener('loadedmetadata', () => {
         video.play();
     })
     videoGrid.append(video);                               //Append the video to be displayed
-}
-
-const connectUser = () => {                                //Function to connect new user 
-    console.log('new user');
 }
